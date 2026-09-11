c2c558b384525869ea35c52ddae0c6ad78f87c0c
Muiz Haruna <hmuiyze@gmail.com>
2026-09-11 06:50:48 +0100

    chore: add Prettier, ESLint, husky pre-commit hook

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    ignores: ["node_modules/"],
+  },
+];
